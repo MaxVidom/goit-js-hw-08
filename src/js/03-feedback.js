066b5e9a05f2548b231b93c207154be0e66ba71c
MaxVidom <MaxVidomenko@gmail.com>
2022-05-12 22:58:07 +0300

    Second and thirth tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('input', throttle(onInputChange, 500));
+refs.form.addEventListener('submit', onFormSubmit)
+
+populateText();
+
+function onInputChange(evt) {
+    const feedbackForm = {
+        email: refs.form.elements.email.value,
+        message: refs.form.elements.message.value,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackForm));
+}
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const feedbackForm = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(feedbackForm);
+    evt.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+function populateText() {
+    const savedText = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (savedText) {
+        refs.form.elements.email.value = savedText.email;
+        refs.form.elements.message.value = savedText.message;
+    };
+}
